b0231f06bbed8518a868d3036a9812a1c5861343
Javi Rodriguez <javi.role1@gmail.com>
2015-03-12 19:21:15 +0100

    -Creado el base.html -El archivo settings.py está configurado para usar una base de datos local, si se quiere usar la base de datos MySQL, desconectar las lineas -Creada la estructura de los archivos estáticos y agradas las dependencias de los archivos .css, .js y fonts, respectivamente.

--- BrainTravel/urls.py
+++ BrainTravel/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'BrainTravel.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    
+    url(r'^signin/$', 'principal.views.viewsModel1.metodo1'),
     #Para indicar en las URLs un metodo que se encuentra dentro de un archivo .py
     # url(r'^<nombre_url>$', '<app>.<paquete>.<archivo>.<metodo>'),
     # url(r'^<nombre_url>$', 'principal.views.ViewsModel1.method1')
--- principal/views/viewsModel1.py
+++ principal/views/viewsModel1.py
@@ -1,3 +1,10 @@
 # -*- coding: latin-1 -*-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render_to_response
+from django.template.context import RequestContext
 
 """Aquí se colocan las vistas relacionadas con el Modelo 1"""
+
+def metodo1(request):
+	print "hola"
+	return render_to_response('signin.html', {}, context_instance=RequestContext(request))
