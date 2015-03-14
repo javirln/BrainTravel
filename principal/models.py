adcd8d68028ed39ae990ce9789b2b57035d95fa6
J.Javier Delgado <dcjosej@gmail.com>
2015-03-14 02:34:03 +0100

    Modelo de dominio Django creado.

--- principal/models.py
+++ principal/models.py
@@ -1,4 +1,211 @@
 # -*- coding: latin-1 -*-
+import datetime
+
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models.fields.related import OneToOneField
+from django.utils import timezone
+from django_enumfield import enum
+
+
+# --------------------------- CUSTOMS VALIDATORS  ----------------------------------
+def PastValidator(value):
+    if value > timezone.now():
+        raise ValidationError(u'' + str(value) + ' Is a future date!')
+# -----------------------------------------------------------------------------------
+
 
 # Create your models here.
+class Administrator(models.Model):
+    user = OneToOneField(User)
+    class Meta:
+        permissions = (
+                       ('administrator', 'Administrator'),
+                       )
+
+class Category(models.Model):
+    id_foursquare = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
+    
+    def __unicode__(self):
+        return self.name 
+
+
+#Conceptually it's abstract, but Django not implement in BD abstract classes
+class Scorable(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    
+    #----------- Derivates -------------------------------#
+    rating = models.FloatField(null=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    
+class Venue(Scorable):
+    id_foursquare = models.CharField(max_length=20, unique=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    photo = models.ImageField(upload_to='static/venue_folder/', null=True, blank=True)
+    phone = models.CharField(max_length=50, null=True)
+    
+    # ------------- Relationships --------------#
+    categories = models.ManyToManyField(Category)
+    
+    def __unicode__(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    description = models.TextField(null=True)
+    leadTime = models.FloatField(validators=[MinValueValidator(1)])
+    duration = models.FloatField(validators=[MinValueValidator(1)])
+    
+    #---------- Derivate ---------------#
+    rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    
+    # ------------- Relationships --------------#
+    traveller = models.ForeignKey('principal.Traveller')
+    venues = models.ForeignKey(Venue)
+    
+    def __unicode__(self):
+        return self.description
+    
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    
+    def __unicode__(self):
+        return self.name
+
+class Trip(Scorable):
+    publishedDescription = models.TextField(null=True)
+    startDate = models.DateField(null=True)
+    endDate = models.DateField(null=True)
+    planified = models.BooleanField()
+    coins = models.IntegerField(validators=[MinValueValidator(0)])
+    
+    #------------- Derivates -----------------------------#
+    likes = models.IntegerField(validators=[MinValueValidator(0)])
+    dislikes = models.IntegerField(validators=[MinValueValidator(0)])
+    
+    # ------------- Relationships --------------#
+    traveller = models.ForeignKey('principal.Traveller')
+    city = models.ForeignKey(City)
+    
+    def __unicode__(self):
+        return 'Ini: ' + self.startDate + 'End: ' + self.endDate
+
+class Traveller(models.Model):
+    
+    Genre = (
+             ('MA', 'MALE'),
+             ('FE', 'FEMALE')
+             )
+    
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    genre = models.CharField(max_length=2, choices=Genre)
+    photo = models.ImageField(upload_to='static/user_folder/', null=True, blank=True)
+    
+    # ----------- Derivates -------------------#
+    reputation = models.FloatField(null=True, blank=True,
+            validators=[MinValueValidator(0), MaxValueValidator(10)])
+    coins = models.IntegerField(validators=[MinValueValidator(0)])
+    recommendations = models.IntegerField(validators=[MinValueValidator(0)])
+    
+    # ------------- Relationships --------------#
+    likedFeedback = models.ManyToManyField(Feedback, through='Likes', related_name='traveller_likes')
+    jugedTrips = models.ManyToManyField(Trip, through='Judges', related_name='judges')
+    commentedTrips = models.ManyToManyField(Trip, through='Comment', related_name='commenters')
+    assessedScorables = models.ManyToManyField(Scorable, through='Assessment')
+    
+    def __unicode__(self):
+        return self.firstName + ' '+ self.lastName
+
+
+class Likes(models.Model):
+    useful = models.BooleanField()
+    comment = models.TextField(null=True)
+    
+    #-------------- Relationships -------------------
+    traveller = models.ForeignKey(Traveller)
+    feedback = models.ForeignKey(Feedback)
+    
+    
+class Day(models.Model):
+    numberDay = models.IntegerField(validators=[MinValueValidator(1)])
+    date = models.DateField(null=True)
+    description = models.TextField(null=True)
+    
+    # ------------- Relationships --------------#
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    venues = models.ManyToManyField(Venue, through='VenueDay')
+    
+    def clean(self):
+        if self in self.trip.day_set:
+            raise ValidationError('The trip already contains this day!')
+    
+class VenueDay(models.Model):
+    order = models.IntegerField(validators=[MinValueValidator(1)])
+    
+    # ------------- Relationships --------------#
+    venue = models.ForeignKey(Venue)
+    day = models.ForeignKey(Day)
+    
+    class Meta:
+        unique_together = ('venue', 'day')
+    
+    
+class Comment(models.Model):
+    description = models.TextField()
+    
+    # ------------- Relationships --------------#
+    traveller = models.ForeignKey(Traveller)
+    trip = models.ForeignKey(Trip)
+    
+class Judges(models.Model):
+    like = models.BooleanField()
+    
+    # ------------- Relationships --------------#
+    traveller = models.ForeignKey(Traveller)
+    trip = models.ForeignKey(Trip, related_name='judgedTrip')
+
+
+class Payment(models.Model):
+    amount = models.FloatField(validators=[MinValueValidator(0)])
+    date = models.DateTimeField(validators=[PastValidator])
+    
+    # ------------- Relationships --------------#
+    traveller = models.ForeignKey(Traveller)
+    
+
+class CoinHistory(models.Model):
+    amount = models.IntegerField()
+    date = models.DateTimeField(validators=[PastValidator])
+    concept = models.TextField()
+    
+    # ------------- Relationships --------------#
+    traveller = models.ForeignKey(Traveller)
+    payment = models.OneToOneField(Payment, null=True, blank=True) 
+    trip = models.OneToOneField(Trip, null=True, blank=True)
+    
+    def __unicode__(self):
+        return str(self.amount)
+    
+
+
+class Schedule(models.Model):
+    day = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)])
+    openingTime = models.CharField(null=True, max_length=128)
+    closingTime = models.CharField(null=True, max_length=128)
+    
+    # ------------- Relationships --------------#
+    venue = models.ForeignKey(Venue)
+    
+class Assessment(models.Model):
+    score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(1)])
+    comment = models.TextField()
+    
+    # ------------- Relationships --------------#
+    traveller = models.ForeignKey(Traveller)
+    scorable = models.ForeignKey(Scorable)
