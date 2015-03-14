2e0629afc10ccdf21bcf130189567cfd7acebb49
J.Javier Delgado <dcjosej@gmail.com>
2015-03-14 02:34:53 +0100

    Admin.py configurado Configurado fichero 'admin.py' para poder utilizar la vista de administración de Django.

--- principal/admin.py
+++ principal/admin.py
@@ -1,4 +1,23 @@
 # -*- coding: latin-1 -*-
 from django.contrib import admin
 
+from principal.models import *
+
+
 # Register your models here.
+admin.site.register(Administrator)
+admin.site.register(Category)
+admin.site.register(Venue)
+admin.site.register(Feedback)
+admin.site.register(City)
+admin.site.register(Trip)
+admin.site.register(Traveller)
+admin.site.register(Likes)
+admin.site.register(Day)
+admin.site.register(VenueDay)
+admin.site.register(Comment)
+admin.site.register(Judges)
+admin.site.register(Payment)
+admin.site.register(CoinHistory)
+admin.site.register(Schedule)
+admin.site.register(Assessment)
