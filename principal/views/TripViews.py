cea2f2eac97be91926c888e288f3b79e44c2391a
Javi Rodriguez <javi.role1@gmail.com>
2015-03-19 10:39:32 +0100

    -Ver detalles de un viaje implementado -Agregado el archivo __init__.py de la carpeta "utils" que hacia que diese error el proyecto

--- principal/views/TripViews.py
+++ principal/views/TripViews.py
@@ -8,16 +8,19 @@ from principal.services import TripService
 
 
 def search_trip(request):
-	if request.method == 'GET':
-		title = request.GET.get('title', False)
-		trip_result = None
-		try:
-			trip_result = TripService.searchTrip(title)
-			return render_to_response('search.html', {'trip_result': trip_result}, context_instance=RequestContext(request))
-		except Exception as e:
-				return HttpResponse(e)
-		
+    if request.method == 'GET':
+        title = request.GET.get('title', False)
+        trip_result = None
+        try:
+            trip_result = TripService.searchTrip(title)
+            return render_to_response('search.html', {'trip_result': trip_result},
+                                      context_instance=RequestContext(request))
+        except Exception as e:
+            return HttpResponse(e)
+
+
 def public_trip_details(request, trip_id):
-	trip = Trip.objects.get(id=trip_id)
-	comments = Comment.objects.get(trip=trip_id)
-	return render_to_response('public_trip_details.html', {'trip': trip, 'comments': comments}, context_instance=RequestContext(request))
+    trip = Trip.objects.get(id=trip_id)
+    comments = Comment.objects.filter(trip=trip_id)
+    return render_to_response('public_trip_details.html', {'trip': trip, 'comments': comments},
+                              context_instance=RequestContext(request))
--- principal/views/UserViews.py
+++ principal/views/UserViews.py
@@ -9,80 +9,84 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http.response import HttpResponse, JsonResponse, \
-	HttpResponseRedirect
+    HttpResponseRedirect
 from django.shortcuts import redirect, render_to_response, render
 from django.template.context import RequestContext
-from django.views.generic.edit import CreateView
 
 from principal.forms import LoginForm, TravellerRegistrationForm
-from principal.models import Traveller
 from principal.services import TravellerService, UserService
 from principal.views import EmailViews
 from principal.utils import BrainTravelUtils
 
-def sign_in(request):
-	registerForm = TravellerRegistrationForm()
-	if request.method == "POST":
-		form = LoginForm(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password']
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				if user.is_active:
-					login(request, user)
-					return redirect("/")
-				else:
-					message = 'Your account is desactivated' 
-					result = render_to_response('signin.html', {'message': message, 'registerForm': registerForm}, context_instance=RequestContext(request))
-			else:
-				message = 'Wrong user or password' 
-				result = render_to_response('signin.html', {'message': message, 'registerForm': registerForm}, context_instance=RequestContext(request))
 
-		else:
-			result = render_to_response('signin.html', {'form': form, 'registerForm': registerForm}, context_instance=RequestContext(request))
-	else:
-		next = request.GET.get('next', '/')
-		result = render_to_response('signin.html', {'next': next, 'registerForm': registerForm}, context_instance=RequestContext(request))
-	
-	return result
+def sign_in(request):
+    registerForm = TravellerRegistrationForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("/")
+                else:
+                    message = 'Your account is desactivated'
+                    result = render_to_response('signin.html', {'message': message, 'registerForm': registerForm},
+                                                context_instance=RequestContext(request))
+            else:
+                message = 'Wrong user or password'
+                result = render_to_response('signin.html', {'message': message, 'registerForm': registerForm},
+                                            context_instance=RequestContext(request))
+
+        else:
+            result = render_to_response('signin.html', {'form': form, 'registerForm': registerForm},
+                                        context_instance=RequestContext(request))
+    else:
+        next = request.GET.get('next', '/')
+        result = render_to_response('signin.html', {'next': next, 'registerForm': registerForm},
+                                    context_instance=RequestContext(request))
+
+    return result
+
 
 @login_required()
-def systemLogout(request):
-	logout(request)
-	return HttpResponseRedirect("/")
+def system_logout(request):
+    logout(request)
+    return HttpResponseRedirect("/")
+
 
 def create_traveller(request):
-	data = request.POST
-	form = TravellerRegistrationForm(data)
-	response = {'success' : form.is_valid()}
-	
-	if form.is_valid():
-		traveller = TravellerService.create(form)
-		rand_password = BrainTravelUtils.id_generator()
-		traveller.set_password(rand_password)
-		traveller.save()
-		
-		EmailViews.send_email_confirmation(traveller, rand_password)
-		# traveller = TravellerService.create(form)
-		# TravellerService.save(traveller)
-	
-	return HttpResponse(json.dumps(response))
+    data = request.POST
+    form = TravellerRegistrationForm(data)
+    response = {'success': form.is_valid()}
+
+    if form.is_valid():
+        traveller = TravellerService.create(form)
+        rand_password = BrainTravelUtils.id_generator()
+        traveller.set_password(rand_password)
+        traveller.save()
+
+        EmailViews.send_email_confirmation(traveller, rand_password)
+    # traveller = TravellerService.create(form)
+    # TravellerService.save(traveller)
+
+    return HttpResponse(json.dumps(response))
+
 
 def confirm_account(request):
-	username = request.GET['username']
-	hash1 = hashlib.sha256(username).hexdigest()
-	hash2 = request.GET['hash']
-	if hash1 == hash2:
-		user = User.objects.get(username=username)
-		user.is_active = True;
-		user.save()
-		
-		password = request.GET['rand_password']
-		user_logged = authenticate(username=user.username, password=password)
-		login(request, user_logged)
-		return HttpResponseRedirect("/")
-	else:
-		return HttpResponse("Hash not math!! :(")
-	
-	
+    username = request.GET['username']
+    hash1 = hashlib.sha256(username).hexdigest()
+    hash2 = request.GET['hash']
+    if hash1 == hash2:
+        user = User.objects.get(username=username)
+        user.is_active = True;
+        user.save()
+
+        password = request.GET['rand_password']
+        user_logged = authenticate(username=user.username, password=password)
+        login(request, user_logged)
+        return HttpResponseRedirect("/")
+    else:
+        return HttpResponse("Hash not math!! :(")
