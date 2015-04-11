439f4f6eb22d2e8b036371f52be62bf44f536df3
david <david>
2015-04-11 14:36:20 +0200

    Corregido el focus al pulsar el label de days añadido control de permisos a la vista de FS. Controlado que el usuario tenga monedas suficientes a la hora de enviar la peticion de crear un plan

--- principal/views/FoursquareViews.py
+++ principal/views/FoursquareViews.py
@@ -3,24 +3,36 @@
 import pprint
 from datetime import date, datetime
 import traceback
+from django.contrib.auth.decorators import permission_required
 
 from django.http import HttpResponse
 from django.shortcuts import redirect, render_to_response
 from django.template.context import RequestContext
 from principal.forms import PlanForm
-from principal.models import Trip
+from principal.models import Trip, Traveller
 from principal.services import TravellerService
 from principal.services import FoursquareServices
 from principal.services.FoursquareServices import init_fs, categories_initializer, search_by_category
 from principal.services.FoursquareServices import init_fs, test_plan
+from principal.utils import BrainTravelUtils
+from principal.views import TripViews
+from principal.views.Coinviews import buy_coins
 
 
 client = init_fs()
 
+
+@permission_required('principal.traveller')
 def show_planning(request, trip_id):
     trip = Trip.objects.get(pk=trip_id)
-    return render_to_response('show_planning.html', {'trip': trip}, context_instance=RequestContext(request))
+    if Traveller.objects.get(id=request.user.id) != trip.traveller:
+        BrainTravelUtils.save_error(request, "This is not your trip")
+        return TripViews.planned_trips(request)
+    else:
+        return render_to_response('show_planning.html', {'trip': trip}, context_instance=RequestContext(request))
 
+
+@permission_required('principal.traveller')
 def foursquare_request(request):
     if request.method == 'GET':
         try:
@@ -32,7 +44,24 @@ def foursquare_request(request):
     else:
         return redirect('/')
 
+# autor: david
+def check_coins(days):
+    if days <= 3:
+        coins = 20
+    elif 3 < days <= 7:
+        coins = 40
+    else:
+        coins = 80
+    return coins
 
+def check_coins_available(traveller, coins_spent):
+    coins_available = traveller.coins
+    if coins_available < coins_spent:
+        return False
+    else:
+        return True
+
+@permission_required('principal.traveller')
 def foursquare_list_venues(request):
     try:
         assert request.user.has_perm('principal.traveller')
@@ -40,8 +69,11 @@ def foursquare_list_venues(request):
         if request.POST:
             form = PlanForm(request.POST)
             if form.is_valid():
-                start_date = form.cleaned_data['startDate']
                 days = int(form.cleaned_data['days'])
+                if check_coins_available(traveller, check_coins(days)) is False:
+                    BrainTravelUtils.save_error(request, "Insufficient coins available")
+                    return buy_coins(request)
+                start_date = form.cleaned_data['startDate']
                 country = form.cleaned_data['country']
                 city = form.cleaned_data['city']
 
@@ -73,15 +105,17 @@ def foursquare_list_venues(request):
                 selected_venues_with_photos = FoursquareServices.save_data(selected_venues)
                 selected_food_with_photos = FoursquareServices.save_data(selected_food)
 
-                trip = FoursquareServices.create_trip(form, request, selected_venues_with_photos, selected_food_with_photos)
+                trip = FoursquareServices.create_trip(form, request, selected_venues_with_photos,
+                                                      selected_food_with_photos)
                 return show_planning(request, trip.id)
             # si no es valido el form devolvemos a editar
-            return render_to_response('plan_creation.html', {'form': form, 'traveller':traveller},
+            return render_to_response('plan_creation.html', {'form': form, 'traveller': traveller},
                                       context_instance=RequestContext(request))
         else:
             form = PlanForm()
-            return render_to_response('plan_creation.html', {'form': form, 'traveller':traveller},
+            return render_to_response('plan_creation.html', {'form': form, 'traveller': traveller},
                                       context_instance=RequestContext(request))
+
     except:
         print traceback.format_exc()
         return render_to_response('error.html', context_instance=RequestContext(request))
