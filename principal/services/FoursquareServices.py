45f58f5d1c66aaf4f8e97ab1d3644dcf7dd0609a
david <david>
2015-04-14 10:34:03 +0200

    Agregada funcion para calcular distancias entre dos puntos segun coordenadas GPS y curvatura de la tierra

--- principal/services/FoursquareServices.py
+++ principal/services/FoursquareServices.py
@@ -12,7 +12,7 @@ import foursquare
 from principal.models import Category, Venue, Trip, Day, VenueDay, CoinHistory
 from principal.services import TravellerService
 from django.db.models.fields import Empty
-
+from math import radians, sin, cos, sqrt, asin
 
 _client_id = "TWYKUP301GVPHIAHBPYFQQT0PJGZ0O2B24HQ3RUGLUFSLP1E"
 _client_secret = "TDNQ441CNLDJZKC3UJYDERT2MNDWN1E2CX1550CW1OXPEST2"
@@ -23,6 +23,19 @@ def init_fs():
     # Construct the client object
     global client
     client = foursquare.Foursquare(client_id=_client_id, client_secret=_client_secret)
+
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+    R = 6372.8  # Earth radius in kilometers
+    dLat = radians(lat2 - lat1)
+    dLon = radians(lon2 - lon1)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    a = sin(dLat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dLon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    # retorna en Km
+    return R * c
 
 
 def categories_initializer():
