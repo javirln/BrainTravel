880f8637ac5fce5a3390b9849b633e5d9cf1420f
Juan Elias <juamaiosu@alum.us.es>
2015-04-29 16:14:10 +0200

    Corregido el acceso a los detalles de un viaje publico

--- principal/views/TripViews.py
+++ principal/views/TripViews.py
@@ -18,11 +18,12 @@ from principal.utils import BrainTravelUtils
 
 # author: Juane
 def search_trip(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-    else:
-        title = request.GET.get('title')
     try:
+        if request.method == 'POST':
+            title = request.POST.get('title')
+        else:
+            title = request.GET.get('title')
+
         trips = TripService.searchTrip(title)
         paginator = Paginator(trips, 2)
         page = request.GET.get('page')
@@ -37,12 +38,12 @@ def search_trip(request):
         return render_to_response('error.html')
 
 
-# author: Javi
+# author: Javi ----- Reviewed: Juane
 def public_trip_details(request, trip_id):
-    trip = Trip.objects.get(id=trip_id)
-    comments = Comment.objects.filter(trip=trip_id)
-    # is_edit = False
-    if trip.traveller.id == request.user.id or trip.state == 'ap' or request.user.has_perm('principal.administrator'):
+    try:
+        trip = Trip.objects.get(id=trip_id)
+        assert trip.traveller.id == request.user.id or trip.state == 'ap' or request.user.has_perm('principal.administrator')
+        comments = Comment.objects.filter(trip=trip_id)
         judges = Judges.objects.filter(trip_id=trip_id, traveller_id=request.user.id)
         assessment = Assessment.objects.filter(scorable_id=trip_id, traveller_id=request.user.id)
         if len(judges) == 0:
@@ -53,15 +54,14 @@ def public_trip_details(request, trip_id):
             assessment = None
         else:
             assessment = list(assessment)[0]
-        # is_edit = True
         data = {'id': trip.id, 'state': trip.state}
         form = TripUpdateStateForm(initial=data)
-        return render_to_response('public_trip_details.html',
-                                  {'trip': trip, 'comments': comments, 'traveller_edit': True,
-                                   'judge': judge, 'form': form, 'assessment': assessment}, context_instance=RequestContext(request))
-    else:
-        BrainTravelUtils.save_error(request)
-        return render_to_response('search.html', {}, context_instance=RequestContext(request))
+        return render_to_response('public_trip_details.html', {'trip': trip, 'comments': comments, 'judge': judge, 'form': form, 'assessment': assessment}, context_instance=RequestContext(request))
+    except AssertionError:
+        BrainTravelUtils.save_error(request, "Access denied")
+        return HttpResponseRedirect('/trip/mylist')
+    except:
+        return render_to_response('error.html')
 
 
 # author: Juane
