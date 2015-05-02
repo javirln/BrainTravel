280cae6ebf9a51ceae15e578cb31d2f4631a38c1
Carlos <cborja@ingenieriacreativa.es>
2015-05-02 21:24:55 +0200

    Múltiples errores de diseño arreglados (ver documento "Errores diseño" compartido en drive.

--- principal/services/CoinService.py
+++ principal/services/CoinService.py
@@ -4,7 +4,7 @@ from principal.models import CoinHistory, Traveller
 
 
 def list_coin_history_traveller(id_traveller):
-    coin_history = CoinHistory.objects.all().filter(traveller=id_traveller)
+    coin_history = CoinHistory.objects.all().filter(traveller=id_traveller).order_by('-date')
     return coin_history
 
 
