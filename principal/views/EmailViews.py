cc726f1784d9a0c089713dccffc4f35e45dccc44
Javi Rodriguez <javi.role1@gmail.com>
2015-05-05 22:03:58 +0200

    Modificado el email de registro

--- principal/views/EmailViews.py
+++ principal/views/EmailViews.py
@@ -7,7 +7,9 @@ import hashlib
 
 def send_email_confirmation(traveller, rand_password):
     hash_confirm = hashlib.sha256(traveller.username).hexdigest()
-    send_templated_email = render_to_string('email/register_confirmation.html', {'hash': hash_confirm, 'username': traveller.username, 'rand_password':rand_password})
-    email = EmailMessage('Asunto importante', send_templated_email, to=[traveller.email])
+    send_templated_email = render_to_string('email/register_confirmation.html',
+                                            {'hash': hash_confirm, 'username': traveller.first_name,
+                                             'rand_password': rand_password})
+    email = EmailMessage('Welcome to BrainTravel', send_templated_email, to=[traveller.email])
     email.content_subtype = "html"
     email.send()
