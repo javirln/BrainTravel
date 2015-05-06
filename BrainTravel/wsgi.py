07e3e4e6348a9fad8ad13fa744d632bf3ed49841
Javi Rodriguez <javi.role1@gmail.com>
2015-05-06 15:16:10 +0200

    test

--- BrainTravel/wsgi.py
+++ BrainTravel/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-
+application = Cling(get_wsgi_application())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BrainTravel.settings")
-application = get_wsgi_application()
