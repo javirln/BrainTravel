6f44df082d4c4278c8871b1d05d9d6edc1e78d28
Javi Rodriguez <javi.role1@gmail.com>
2015-05-06 18:56:47 +0200

    Arreglado el envio de correos

--- principal/views/EmailViews.py
+++ principal/views/EmailViews.py
@@ -8,7 +8,8 @@ import hashlib
 def send_email_confirmation(traveller, rand_password):
     hash_confirm = hashlib.sha256(traveller.username).hexdigest()
     send_templated_email = render_to_string('email/register_confirmation.html',
-                                            {'hash': hash_confirm, 'username': traveller.first_name,
+                                            {'hash': hash_confirm, 'username': traveller.username,
+                                             'name': traveller.first_name,
                                              'rand_password': rand_password})
     email = EmailMessage('Welcome to BrainTravel', send_templated_email, to=[traveller.email])
     email.content_subtype = "html"
